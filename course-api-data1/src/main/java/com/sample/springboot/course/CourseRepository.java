1a2bf4c49d83dd322c4daaeb9477c914b0bf53ac
shashimamillapalli <shreeshashi.ss@gmail.com>
2020-06-22 17:59:49 +0530

    course-api-project

---
+++ course-api-data1/src/main/java/com/sample/springboot/course/CourseRepository.java
@@ -0,0 +1,12 @@
+package com.sample.springboot.course;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String> {
+
+	public List<Course> findByTopicId(String topicId);
+	
+	
+}
---
+++ course-api-data1/src/main/java/com/sample/springboot/course/CourseService.java
@@ -0,0 +1,42 @@
+package com.sample.springboot.course;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+	
+	@Autowired
+	private CourseRepository courseRepository;
+
+	public List<Course> getAllCourses(String topicId) {
+		List<Course> course = new ArrayList<Course>();
+		courseRepository.findByTopicId(topicId).forEach(course::add);
+		return course;
+	}
+
+	public Optional<Course> getCourse(String id) {
+		//return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return courseRepository.findById(id);
+	}
+
+	public void addCourse(Course course) {
+		courseRepository.save(course); 
+		
+	}
+
+	public void updateCourse(Course course) {
+		courseRepository.save(course);
+		
+	}
+
+	public void deleteCourse(String id) {
+		courseRepository.deleteById(id);
+	}
+
+}
